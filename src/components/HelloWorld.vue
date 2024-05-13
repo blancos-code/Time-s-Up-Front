df9eb9fc4f03888ddc631dfea24985597b8cc524
DyaZix-Code <blancosacha38@gmail.com>
2024-05-13 11:30:44 +0200

    feat(taches): html css initialisé

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import StackedHome from "./application-ui/page-examples/home-screens/stacked-home.vue";
+import StackedDetails from "./application-ui/page-examples/detail-screens/stacked-details.vue";
 defineProps<{ msg: string }>()
 
 const count = ref(0)
 </script>
 
 <template>
-  <stacked-home></stacked-home>
+<!--  <stacked-home></stacked-home> -->
+  <stacked-details></stacked-details>
 </template>
 
 <style scoped>
