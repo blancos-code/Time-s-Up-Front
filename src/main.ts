fcbdaf262fa8cd71154812b42b1ed2d132d44989
Estelle JOYEUX BOUILLON <estelle.joyeuxbouillon@hardis-group.com>
2024-05-13 14:09:07 +0200

    start rooting (not working yet but sacha wants me to push)

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,24 @@ import './style.css'
 import PrimeVue from 'primevue/config';
 import App from './App.vue'
 import Lara from './assets/presets/aura';
+import { createMemoryHistory, createRouter } from 'vue-router'
 
-const app = createApp(App)
+import HomeView from './components/HelloWorld.vue'
+import Task from './components/TaskView.vue';
+import Test from './components/application-ui/page-examples/settings-screens/stacked.vue';
+const routes = [
+  { path: '/', component: HomeView },
+  {path: '/test', component: Test},
+  { path: '/task', component: Task },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+const app = createApp(App);
+app.use(router)
 
 app.use(PrimeVue, {
     unstyled: true,
