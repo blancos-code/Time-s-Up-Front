5c7468635d9607285ddc6c98a755061f14905033
DyaZix-Code <blancosacha38@gmail.com>
2024-05-13 11:18:48 +0200

    feat(dashboard): html css du dashboard créé

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import StackedHome from "./application-ui/page-examples/home-screens/stacked-home.vue";
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <stacked-home></stacked-home>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
