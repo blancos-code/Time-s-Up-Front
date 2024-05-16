a30ce342dd29c4d3e3cc4470e0065768f994c687
DyaZix-Code <blancosacha38@gmail.com>
2024-05-16 10:02:35 +0200

    fix(auth): si 401 on supprime le authToken car il  n'est plus valable

--- src/config/api/axios.ts
+++ src/config/api/axios.ts
@@ -1,6 +1,36 @@
 import axios from 'axios';
+import router from "../../router";
 
 export const httpClient = axios.create({
     baseURL: 'https://4260-37-170-120-65.ngrok-free.app/',
     timeout: 5000,
 });
+
+httpClient.interceptors.request.use(config => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    } else {
+        router.push({ name: 'Login' });
+    }
+
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+
+
+httpClient.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response && error.response.status === 401) {
+
+            localStorage.removeItem('accessToken');
+            router.push({ name: 'Login' });
+
+            return Promise.reject(error);
+        }
+
+        return Promise.reject(error);
+    }
+);
